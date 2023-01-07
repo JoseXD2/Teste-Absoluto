d43a5c9a241c745ecc97ca0cb0946a52e0a4c951
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-07 14:44:45 -0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -143,8 +143,8 @@ class Character extends FlxSprite
 				//var modTextureToFind:String = Paths.modFolders("images/"+json.image);
 				//var textureToFind:String = Paths.getPath('images/' + json.image, new AssetType();
 				
-				if (HSys.exists(modAnimToFind) || HSys.exists(animToFind || Assets.exists(animToFind)))
-				
+				if (HSys.exists(modAnimToFind) || HSys.exists(animToFind) || Assets.exists(animToFind))
+					
 				if (Assets.exists(Paths.getPath('images/' + json.image + '/Animation.json', TEXT)))
 				
 				{
